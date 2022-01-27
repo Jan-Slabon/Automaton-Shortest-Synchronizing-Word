153c5a3c21b2c1f7978fd0813cc0fa8882b1d143
Jan-Slabon <Jan-Slabon@users.noreply.github.com>
2022-01-27 17:33:05 +0100

    debugged version of bfs

--- synchronizing.hs
+++ synchronizing.hs
@@ -33,15 +33,19 @@ transitionInPowerAutomata :: [Function]->Char->[Integer]->[Integer]
 transitionInPowerAutomata f a = map (transition f a) 
 
 type Trace = ([Integer],String)
-
+collapse_ :: [Integer] -> [Integer] ->[Integer]
+collapse_ [] tmp = tmp
+collapse_ (s:xs) tmp = collapse_ (filter (/=s) xs) $ s:tmp
+collapse :: [Integer] -> [Integer]
+collapse l =  collapse_ l []
 singleStateTrace :: [Trace] -> String
 singleStateTrace [] = "None"
 singleStateTrace ((node, s):xs)
-                    | ((length node) == 1) = s
+                    | ((length $ collapse node) == 1) = s
                     | otherwise = singleStateTrace xs
 isSingleState :: Bool -> Trace -> Bool
 isSingleState prev (node,s)
-                | ((length node) == 1) = True
+                | ((length $ collapse node) == 1) = True
                 | otherwise = (prev || False)
 
 bfs :: String -> [Function] -> [Trace] -> String
@@ -49,3 +53,7 @@ bfs alfabet func start
             | isReached = singleStateTrace start 
             | otherwise = bfs alfabet func $ concat $ map (\a -> map (\(li, s) -> (transitionInPowerAutomata func a li, a:s)) start) alfabet 
             where isReached = foldl isSingleState False start 
+
+--func = [(1,'b',2),(1,'a',1),(2,'b',2),(2,'a',1)]
+--alfabet = "ab"
+--start = [([1,2],"")]
